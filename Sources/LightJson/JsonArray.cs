78cc9e0bb5f0c5b95aa64e9baf216c95c7e4121a
Marcos Lopez C <MarcosLopezC@live.com>
2015-02-21 15:48:26 -0500

    Adding JsonArray implementation.

--- Sources/LightJson/JsonArray.cs
+++ Sources/LightJson/JsonArray.cs
@@ -1,8 +1,77 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LightJson
 {
-	public sealed class JsonArray
+	public sealed class JsonArray : IEnumerable<JsonValue>
 	{
+		private IList<JsonValue> items;
+
+		public int Count
+		{
+			get
+			{
+				return this.items.Count;
+			}
+		}
+
+		public JsonValue this[int index]
+		{
+			get
+			{
+				return this.items[index];
+			}
+			set
+			{
+				this.items[index] = value;
+			}
+		}
+
+		public JsonArray()
+		{
+			this.items = new List<JsonValue>();
+		}
+
+		public JsonArray(params JsonValue[] values) : this()
+		{
+			foreach (var value in values)
+			{
+				this.items.Add(value);
+			}
+		}
+
+		public JsonArray Add(JsonValue value)
+		{
+			this.items.Add(value);
+			return this;
+		}
+
+		public JsonArray Insert(int index, JsonValue value)
+		{
+			this.items.Insert(index, value);
+			return this;
+		}
+
+		public JsonArray Remove(int index)
+		{
+			this.items.RemoveAt(index);
+			return this;
+		}
+
+		public JsonArray Clear()
+		{
+			this.items.Clear();
+			return this;
+		}
+
+		public IEnumerator<JsonValue> GetEnumerator()
+		{
+			return this.items.GetEnumerator();
+		}
+
+		System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+		{
+			return this.GetEnumerator();
+		}
 	}
 }
