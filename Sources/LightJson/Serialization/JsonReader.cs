c1e65f6e64326f7599267abea1d7d1a90efeb50a
MarcosLopezC <MarcosLopezC@live.com>
2015-04-20 11:21:19 -0400

    Adding unicode hexadecimal parsing functionality.

--- Sources/LightJson/Serialization/JsonReader.cs
+++ Sources/LightJson/Serialization/JsonReader.cs
@@ -289,9 +289,73 @@ namespace LightJson.Serialization
 			return builder.ToString();
 		}
 
-		private string ReadUnicodeLiteral()
+		private int ReadHexDigit()
 		{
-			throw new NotImplementedException();
+			switch (char.ToUpper(Read()))
+			{
+				case '0':
+					return 0;
+
+				case '1':
+					return 1;
+
+				case '2':
+					return 2;
+
+				case '3':
+					return 3;
+
+				case '4':
+					return 4;
+
+				case '5':
+					return 5;
+
+				case '6':
+					return 6;
+
+				case '7':
+					return 7;
+
+				case '8':
+					return 8;
+
+				case '9':
+					return 9;
+
+				case 'A':
+					return 10;
+
+				case 'B':
+					return 11;
+
+				case 'C':
+					return 12;
+
+				case 'D':
+					return 13;
+
+				case 'E':
+					return 14;
+
+				case 'F':
+					return 15;
+
+				default:
+					throw new ArgumentException("The given string does not represent a hex value.", "hex");
+			}
+		}
+
+		private char ReadUnicodeLiteral()
+		{
+			int value = 0;
+
+			value += ReadHexDigit() * 4096; // 16^3
+			value += ReadHexDigit() * 256;  // 16^2
+			value += ReadHexDigit() * 16;   // 16^1
+			value += ReadHexDigit();        // 16^0
+
+			return (char)value;
 		}
 
 		private JsonObject ReadObject()
