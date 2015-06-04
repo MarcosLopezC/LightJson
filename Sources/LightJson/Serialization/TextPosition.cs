707bef2ad5543d05884add2333b55c3f9284828c
MarcosLopezC <MarcosLopezC@live.com>
2015-06-04 13:37:44 -0400

    Adding TextPosition struct.

---
+++ Sources/LightJson/Serialization/TextPosition.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LightJson.Serialization
+{
+	/// <summary>
+	/// Represents a position within a plain text resource.
+	/// </summary>
+	public struct TextPosition
+	{
+		/// <summary>
+		/// The column position, 0-based.
+		/// </summary>
+		public long column;
+
+		/// <summary>
+		/// The line position, 0-based.
+		/// </summary>
+		public long line;
+	}
+}
