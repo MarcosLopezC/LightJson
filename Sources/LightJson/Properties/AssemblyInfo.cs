c56d99fe666a032e769fea0e0e8caa2e4d9905d4
MarcosLopezC <MarcosLopezC@live.com>
2018-03-08 20:53:20 -0500

    Updating copyright year

--- Sources/LightJson/Properties/AssemblyInfo.cs
+++ Sources/LightJson/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Marcos López C.")]
 [assembly: AssemblyProduct("LightJson")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
