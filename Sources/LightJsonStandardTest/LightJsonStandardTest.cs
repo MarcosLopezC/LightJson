5e76fecc8d4ca7a2e2c52b1bfec35448582847b2
skia <saiedkia@outlook.com>
2018-10-05 20:59:45 +0330

    added nuget package

--- Sources/LightJsonStandardTest/LightJsonStandardTest.cs
+++ Sources/LightJsonStandardTest/LightJsonStandardTest.cs
@@ -8,10 +8,34 @@ namespace LightJsonStandardTest
     public class LightJsonStandardTest
     {
         [TestMethod]
-        public void should_return_parsed_double()
+        public void AsNumber_should_return_parsed_double()
         {
             double result = new JsonValue("0.5").AsNumber;
             Assert.AreEqual(result, 0.5);
+        }
+
+
+        [TestMethod]
+        public void AsString_should_return_equal_string()
+        {
+            string result = new JsonValue("0.5").AsString;
+            Assert.AreEqual(result, "0.5");
+        }
+
+        [TestMethod]
+        public void Desrialzie_JsonObject()
+        {
+            string deserilizationReuslt = new JsonObject()
+                .Add("menu", new JsonArray()
+                .Add("home")
+                .Add("projects")
+                .Add("about")).ToString(true).Replace('\n', ' ').Replace('\r', ' ').Replace('\t', ' ').Replace(" ", "");
+
+
+            string expectedResult = "{\"menu\": [  \"home\", \"projects\", \"about\" ] }".Replace('\n', ' ').Replace('\r', ' ').Replace('\t', ' ').Replace(" ", "");
+
+
+            Assert.AreEqual(deserilizationReuslt, expectedResult);
         }
 
         // incompleted
